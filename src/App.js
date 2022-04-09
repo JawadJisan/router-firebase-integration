01a1c8d8ddffb00b4fbcce42e75d065dc5238161
JawadJisan <jisan.sc@gmail.com>
2022-04-09 10:54:09 +0600

    finish with JH

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
+import Orders from './components/Orders/Orders'
+import RequireAuth from './components/RequireAuth/RequireAuth'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
-        <Route path='register' element={<Register/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='orders' element={
+          <RequireAuth>
+            <Orders/>
+          </RequireAuth>
+        }></Route>
       </Routes>
      
     </div>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,40 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+// import useFirebase from '../../hooks/useFirebase';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    const {signInWithGoogle} = useFirebase();
+    // const {signInWithGoogle} = useFirebase();
+
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation()
+    const navigate = useNavigate();
+
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIni = () =>{
+        signInWithGoogle()
+        .then(()=>{
+            navigate(from, { replace: true })
+        })
+    }
+
     return (
         <div>
             <h2>Please Login</h2>
-            <div style={{margin:'20px'}}>
-            <button onClick={signInWithGoogle}>Google Sign in</button>
+            <div style={{ margin: '20px' }}>
+             {/* <button onClick={signInWithGoogle}>Google Sign in</button> */}
+                
+
+            <button onClick={handleGoogleSignIni}>Google Sign in</button>
+
             </div>
             <form>
-               
+
                 <input type="email" placeholder='Your Email' />
                 <br />
                 <input type="password" name="" placeholder='Enter Your Password' id="" />
